e0002439f8f9a72e7b5488d2b1093737df3efb46
ajhykel <ajhykel@gmail.com>
2013-06-17 11:33:10 +0700

    added init

---
+++ init.rb
@@ -0,0 +1,23 @@
+
+quit = nil
+while quit != 'y' do
+
+puts "Enter amount for rent:"
+rent = gets.chomp.to_f
+
+puts "Enter total of squars:"
+squares = gets.chomp.to_f
+
+puts "Enter hours:"
+hours = gets.chomp.to_f
+minutes = hours * 60
+
+
+print "Your room value per square is: "
+room_value = rent / minutes / squares
+puts room_value
+
+puts "Would you like to Exit: y or n?"
+quit = gets.chomp.strip
+
+end
